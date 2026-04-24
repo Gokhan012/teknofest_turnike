84cc0430cb7934ec3481475a6a4decb0c272f66a
Gokhan012 <64339586+Gokhan012@users.noreply.github.com>
2026-04-24 14:43:41 +0300

    yorum satırı güncellemesi
    Removed comments in Turkish from init_db.py

--- init_db.py
+++ init_db.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-# Yeni ve temiz bir dosya oluşturur
 conn = sqlite3.connect('turnike.db')
 cursor = conn.cursor()
 
-# Tabloyu oluştur (Vektörleri BLOB olarak saklayacağız)
 cursor.execute('''
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
